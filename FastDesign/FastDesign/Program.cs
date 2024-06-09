c5143187016091b212c7cda7cf8f9b0bb879889d
Eric Castro <eric.castro.ingenieria@gmail.com>
2024-06-09 03:49:55 -0300

    Edit Plugin

---
+++ FastDesign/FastDesign/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Autodesk.Revit.UI;
+using Autodesk.Revit.DB;
+using Autodesk.Revit.Attributes;
+
+namespace FastDesign
+{
+
+    public class Program : IExternalApplication
+    {
+        public Result OnShutdown(UIControlledApplication application)
+        {
+            return Result.Succeeded;
+        }
+
+        public Result OnStartup(UIControlledApplication application)
+        {
+            application.CreateRibbonTab("FastDesign"); //Principal menu
+
+                application.CreateRibbonPanel("FastDesign", "Edit Level"); //Second menu
+
+                application.CreateRibbonPanel("FastDesign", "New Menu"); //Second menu
+
+                application.CreateRibbonPanel("FastDesign", "New Menu1"); //Second menu
+
+            return Result.Succeeded;
+        }
+    }
+}
